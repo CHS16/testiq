7002676d9c6dda84c5a512ac8c54557cace358ed
shubham <shubhamsaurav@SHUBHAMs-MacBook-Air-2.local>
2026-04-04 18:48:26 +0530

    ..  commit

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,6 +3,5 @@ from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
-
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Shubham:AkWdclGl8egae8fr@projectdb.1ad82kx.mongodb.net/intent_iq_db?retryWrites=true&w=majority&appName=ProjectDB")
 DB_NAME = os.getenv("DB_NAME", "intent_iq_db")
